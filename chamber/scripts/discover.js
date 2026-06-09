0d8e747c419d733b2bdbaead6cc07ab7de5c784a
kofibill <williamosiakwan96@gmail.com>
2026-06-09 22:09:51 +0000

    update

--- chamber/scripts/discover.js
+++ chamber/scripts/discover.js
@@ -54,13 +54,34 @@ function checkVisitorHistory(){
     const currentTime = Date.now();
     //determine the logic based on if they have visited the page or not
     const lastVisit = localStorage.getItem('lastWebpageVisit');
-
+    //no initial visits displays this
     if (!lastVisit){
         message.textContent = 'Welcome! Let us know if you have any questions.';
     } else {
         // Returning visitor - do the math
         const timeDifference = currentTime - parseInt(lastVisit);
-        const oneDayInMilliseconds = 24 * 60 * 60 * 1000;
+        const oneDayInMilliseconds = 24 * 60 * 60 * 1000;// this is for the time in a whole day
+        const daysBetween = Math.floor(timeDifference / oneDayInMilliseconds);
 
+        if(timeDifference < oneDayInMilliseconds){
+            message.textContent = 'You came back so soon! That\'s Awesome!';
+        } else if(daysBetween === 1){
+            message.textContent = `You visited this page ${daysBetween} one day ago!`;
+        } else {
+            message.textContent = `You visited this page ${daysBetween} days ago!`;
+        }
     }
+    //showing the banner with the message in it
+    container.style.display = 'block';
+    //Update the localStorage box with today's date for next time
+    localStorage.setItem('lastWebpageVisit', currentTime);
 }
+// Run the script automatically as soon as the page loads
+    window.onload = checkVisitorHistory;
+
+// Function to close the message area when "X" is clicked
+    const closeButton = document.getElementById('close-banner');
+    if (closeButton){
+        closeButton.addEventListener('click',closeBanner());
+    }
+
